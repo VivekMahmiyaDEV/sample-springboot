6e8ecaee1965449516f026a5da465c4e346a8750
Vivek Mahmiya <vmahmiya@webmd.net>
2022-08-27 14:31:31 +0530

    Test Sample Project First Commit

---
+++ src/main/java/com/sample/springbootdemo/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.sample.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class SampleController {
+
+    @GetMapping(path = {"/sample","/sample/{name}"})
+    public String testsample(@PathVariable("name") Optional<String> name) {
+        return name.map(n -> "Hello " + n).orElse("Hello World");
+    }
+}
---
+++ src/test/java/com/sample/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sample.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+
+}
